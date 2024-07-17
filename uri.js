64c9060f049a1440b4d10658061b89fbb17c123c
JaguarsCodehub <techbrains21@gmail.com>
2024-07-17 17:53:38 +0530

    Feature: Image Updates

---
+++ uri.js
@@ -0,0 +1,10 @@
+const fixAndroidUri = (uri) => {
+    // Fix Android URI format if necessary
+    if (uri.startsWith('file:/')) {
+        return `file:///${uri.split('file:/').join('')}`;
+    }
+    return uri;
+
+};
+
+console.log(fixAndroidUri('file:///data/user/0/com.techbrains.society/cache/ImagePicker/38565588-59b3-47a7-9db4-1779c35c4d4d.jpeg'))
